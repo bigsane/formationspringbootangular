b8d9e2564bdd6396ea0e6198b61eb555f7ba8cbc
Pierre Cailleaud <pierre.cailleaud@gmail.com>
2020-08-14 16:23:46 +0200

    6-AmeliorationAPI

--- parking/src/main/java/com/formation/parking/services/impl/ParkingServiceImpl.java
+++ parking/src/main/java/com/formation/parking/services/impl/ParkingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.formation.parking.services.impl;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,13 +31,34 @@ public class ParkingServiceImpl implements ParkingService {
 		for (RecordEntity record : reponse.getRecords()) {
 			Parking parking = new Parking();
 			parking.setNom(record.getFields().getGrpNom());
-			parking.setStatut(record.getFields().getGrpStatut());
+			parking.setStatut(getLibelleStatut(record));
 			parking.setNbPlacesDispo(record.getFields().getGrpDisponible());
 			parking.setNbPlacesTotal(record.getFields().getGrpExploitation());
-			parking.setHeureMaj(record.getFields().getGrpHorodatage());
+			parking.setHeureMaj(getHeureMaj(record));
 			resultat.add(parking);
 		}
 		return resultat;
+	}
+
+	private String getHeureMaj(RecordEntity record) {
+		OffsetDateTime dateMaj = OffsetDateTime.parse(record.getFields().getGrpHorodatage());
+		OffsetDateTime dateMajWithOffsetPlus2 = dateMaj.withOffsetSameInstant(ZoneOffset.of("+02:00"));
+		return dateMajWithOffsetPlus2.getHour() + "h" + dateMajWithOffsetPlus2.getMinute();
+	}
+
+	private String getLibelleStatut(RecordEntity record) {
+		switch (record.getFields().getGrpStatut()) {
+			case "1": {
+				return "FERME";
+			}
+			case "2": {
+				return "ABONNES";
+			}
+			case "5": {
+				return "OUVERT";
+			}
+		}
+		return "Données non disponibles";
 	}
 
 }
