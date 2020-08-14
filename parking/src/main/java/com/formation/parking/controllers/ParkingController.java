125c42394e7743eda828a04bd3daf8c508b3bbe6
Pierre Cailleaud <pierre.cailleaud@gmail.com>
2020-08-14 16:20:11 +0200

    2-CreationController

---
+++ parking/src/main/java/com/formation/parking/controllers/ParkingController.java
@@ -0,0 +1,28 @@
+package com.formation.parking.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.parking.models.Parking;
+
+@RestController
+public class ParkingController {
+
+	@RequestMapping(path = "/api/parkings", method = RequestMethod.GET)
+	public List<Parking> getListeParkings() {
+		Parking parkingTest = new Parking();
+		parkingTest.setNom("Parking de test");
+		parkingTest.setNbPlacesTotal(300);
+		parkingTest.setNbPlacesDispo(125);
+		parkingTest.setStatus("OUVERT");
+		parkingTest.setHeureMaj("20h15");
+		
+		ArrayList<Parking> liste = new ArrayList<Parking>();
+		liste.add(parkingTest);
+		return liste;
+	}
+}
